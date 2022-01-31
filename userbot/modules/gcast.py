1fe9052d6d1f1706b362d4247cf06b9eeb7fbdfb
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-01-31 23:29:38 +0700

    nasty

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -27,6 +27,7 @@ GCAST_BLACKLIST = [
     -1001109837870,  # TelegramBotIndonesia
     -1001752592753,  # Skyzusupport
     -1001788983303,  # KayzuSupport
+    -1001380293847,  # NastySupport
 ]
 
 
