be5411bf0de8790370b8ee771dfcdbc32817670b
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-18 15:19:09 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -292,11 +292,11 @@ S_PACK_NAME = os.environ.get("S_PACK_NAME", None)
 
 # Default .alive Logo
 ALIVE_LOGO = os.environ.get(
-    "ALIVE_LOGO") or "https://telegra.ph/file/d257ba98dbd40a7f6fa04.jpg"
+    "ALIVE_LOGO") or "https://telegra.ph/file/950d5fe4dd1dbe1726f5e.jpg"
 
 # Default .helpme Logo
 INLINE_PIC = os.environ.get(
-    "INLINE_PIC") or "https://telegra.ph/file/d257ba98dbd40a7f6fa04.jpg"
+    "INLINE_PIC") or "https://telegra.ph/file/950d5fe4dd1dbe1726f5e.jpg"
 
 # Default emoji help
 EMOJI_HELP = os.environ.get("EMOJI_HELP") or "✨"
